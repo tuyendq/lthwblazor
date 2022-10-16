1c7cf5cea20c2bd82ac88879baea4a1d4676c5f7
Tuyen Dang <tuyendq@gmail.com>
2022-10-16 22:40:08 +0700

    Add model: PizzaTopping.cs

---
+++ BlazingPizzaSite/Model/PizzaTopping.cs
@@ -0,0 +1,8 @@
+namespace BlazingPizzaSite;
+
+public class PizzaTopping
+{
+    public Topping? Topping { get; set; }
+    public int ToppingId { get; set; }
+    public int PizzaId { get; set; }
+}
