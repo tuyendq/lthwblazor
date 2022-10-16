862350ac6f5ede22350cea8e78adcc9b4c14d2bb
Tuyen Dang <tuyendq@gmail.com>
2022-10-16 22:36:32 +0700

    Add model: Topping.cs

---
+++ BlazingPizzaSite/Model/Topping.cs
@@ -0,0 +1,9 @@
+namespace BlazingPizzaSite;
+
+public class Topping
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string GetFormattedPrice() => Price.ToString("0.00");
+}
